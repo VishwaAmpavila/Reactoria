4bc3b8989aeba2743c973cbe769526d606c62956
VishwaAmpavila <150771071+VishwaAmpavila@users.noreply.github.com>
2025-01-12 22:09:46 +0530

    Added the basic layout for the UI of the main menu and the difficulty selection page.

---
+++ scripts/difficulty_selection.gd
@@ -0,0 +1,16 @@
+extends Control
+
+func _on_easy_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+
+func _on_medium_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_hard_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_adaptive_pressed() -> void:
+	pass # Replace with function body.
